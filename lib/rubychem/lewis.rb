36a57848d9c695e10efcd19d875d0d916dd4e43e
Justin Zollars <fogonthedowns@gmail.com>
2013-02-05 00:43:37 -0800

    better

--- lib/rubychem/lewis.rb
+++ lib/rubychem/lewis.rb
@@ -1,42 +1,40 @@
 module RubyChem
- class Lewis
-	attr_accessor :structure
-  Valence = {"O" => 6, "C" => 4}
-  Species = [["C"], ["O", "2"]]
+  class Lewis
+    attr_accessor :structure
+    Valence = {"O" => 6, "C" => 4}
+    Species = [["C"], ["O", "2"]]
   
-	def initialize
-		@species = Species
-		 determine_configuration
-	end
-	
-	private 
-
-	def determine_configuration
-		@valence_electrons_available = valence_electrons_available(@species)
-		@valence_electrons_needed = valence_electrons_needed_to_complete_shell(@species) * 8
-		@shared_electrons = @valence_electrons_needed - @valence_electrons_available 
-	end
-	
-	def valence_electrons_available(species)
-	  electrons = []
-	  species.each do |x|
-	    if x.count > 1
-	      x.count.times do
-	        electrons << Valence[x.first] 
-        end
-      else
-        electrons << Valence[x.first]
-      end	    
+    def initialize
+      @species = Species
+      determine_configuration
     end
-    electrons.inject{|sum,x| sum + x }
-  end
+	
+    private 
   
-  def valence_electrons_needed_to_complete_shell(species)
-    array = []
-    species.each{|x|array << x.count}
-    array.inject{|sum,x| sum + x}
+    def determine_configuration
+      @valence_electrons_available = valence_electrons_available(@species)
+      @valence_electrons_needed = valence_electrons_needed_to_complete_shell(@species) * 8
+      @shared_electrons = @valence_electrons_needed - @valence_electrons_available 
+    end
+	
+    def valence_electrons_available(species)
+      electrons = []
+      species.each do |x|
+        if x.count > 1
+          x.count.times do
+	    electrons << Valence[x.first] 
+          end
+        else
+          electrons << Valence[x.first]
+        end	    
+      end
+      electrons.inject{|sum,x| sum + x }
+    end
+  
+    def valence_electrons_needed_to_complete_shell(species)
+      array = []
+      species.each{|x|array << x.count}
+      array.inject{|sum,x| sum + x}
+    end
   end
-   
- 
- end
 end
